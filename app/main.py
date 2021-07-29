b94c0dd5ca05b2506ba3e26fc7148da9366ff77c
JustinKasteleijn <48954608+JustinKasteleijn@users.noreply.github.com>
2021-07-29 09:57:20 +0200

    Added a main class to run the python game

--- app/main.py
+++ app/main.py
@@ -1,12 +1,22 @@
 import pygame
 from pygameData.Screen import Screen
 
-screen = Screen(500, 500)
-screen.fill((255, 255, 0))
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            pygame.quit()
+class App:
+    def __init__(self):
+        self.running = False
+        pygame.init()
+
+    def run(self):
+        screen = Screen(500, 500)
+        screen.fill((255, 255, 0))
+        self.running = True
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                    pygame.quit()
+
+
+if __name__ == "__main__":
+    App().run()
